b3bfe42fc1c796407d7bdd073607455a1703533a
ericsdata <eachristiansen1@gmail.com>
2021-04-21 21:44:08 -0400

    Infinite Scroll blocked

--- Code/millerScrape.py
+++ Code/millerScrape.py
@@ -5,33 +5,14 @@ Script written to scrape speech transcripts from Miller Center repository
 HTML notes
 
 
+
+
 view-source:https://millercenter.org/the-presidency/presidential-speeches
 
-- NAMES
 
-<form class="views-exposed-form bef-exposed-form" data-drupal-selector="views-exposed-form-presidential-speech-presidential-speech-view-block" action="/the-presidency/presidential-speeches" method="get" id="views-exposed-form-presidential-speech-presidential-speech-view-block" accept-charset="UTF-8">
-  <fieldset data-drupal-selector="edit-field-president-target-id" id="edit-field-president-target-id--wrapper" class="fieldgroup form-composite js-form-item form-item js-form-wrapper form-wrapper">
-      <legend>
-    <span class="fieldset-legend">President</span>
-  </legend>
-  <div class="fieldset-wrapper">
-            <div id="edit-field-president-target-id" class="form-checkboxes"><div class="form-checkboxes">
-                  <div class="js-form-item form-item js-form-type-checkbox form-item-field-president-target-id-44 js-form-item-field-president-target-id-44">
-        <input data-drupal-selector="edit-field-president-target-id-44" type="checkbox" id="edit-field-president-target-id-44" name="field_president_target_id[44]" value="44" class="form-checkbox" />
+- SPEECHES 
 
-        <label for="edit-field-president-target-id-44" class="option">George Washington</label>
-      </div>
-
-                    <div class="js-form-item form-item js-form-type-checkbox form-item-field-president-target-id-45 js-form-item-field-president-target-id-45">
-        <input data-drupal-selector="edit-field-president-target-id-45" type="checkbox" id="edit-field-president-target-id-45" name="field_president_target_id[45]" value="45" class="form-checkbox" />
-
-        <label for="edit-field-president-target-id-45" class="option">John Adams</label>
-      </div>
-
-
-      
-
-- SPEECHES
+## Will need to engage this infinite scroll =>  https://medium.com/@harshvb7/scraping-from-a-website-with-infinite-scrolling-7e080ea8768e
 
   <div data-drupal-views-infinite-scroll-content-wrapper class="views-infinite-scroll-content-wrapper clearfix">    <div class="views-row"><div class="views-field views-field-title"><span class="field-content"><a href="/the-presidency/presidential-speeches/january-20-2021-inaugural-address" hreflang="en">January 20, 2021: Inaugural Address</a></span></div><div class="views-field views-field-presidential-media-icons"><span class="field-content"><section class="speech-icons-column">
             <span class="media-video">video icon</span>
@@ -57,11 +38,34 @@ view-source:https://millercenter.org/the-presidency/presidential-speeches
                 <span class="media-transcript">transcript icon</span>
 
 
+
+
+view-sorce: /presidential-speeches/january-19-2021-farewell-address
+
+pull meta data here
+
+<div  class="about-this-episode">
+    <h3>About this speech</h3>
+      
+<div  class="about-this-episode--inner">
+      
+            <p class="president-name">Donald Trump</p>
+      
+            <p class="episode-date">January 19, 2021</p>
+
+TRANSCRIPT
+
 '''
 
 import requests
 from bs4 import BeautifulSoup
 
 #site of list of all president names, need to a
-url = https://millercenter.org/the-presidency/presidential-speeches
-page = 
+url = "https://millercenter.org/the-presidency/presidential-speeches"
+
+page = requests.get(url)
+
+soup = BeautifulSoup(page.content,'html.parser')
+
+## Parse through HREF - append url to direct to each speech page
+f = soup.findall(class_ = 'views-row')
